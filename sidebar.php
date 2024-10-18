3efd243832f35a2696970c94f2968a8b42d5fdb2
Johanso <johanricardo4@gmail.com>
2024-10-18 04:42:15 -0500

    fx style

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <div id="sidebar-float" class="sidebar__float">
   <div class="sidebar__float-inner">
     <i class="icon-filter-left"></i>
-    <span>Sidebar</span>
+    <span>Menú JavaScript</span>
   </div>
 </div>
 <?php dynamic_sidebar('sidebar-post'); ?>
